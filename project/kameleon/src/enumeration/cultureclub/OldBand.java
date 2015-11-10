3a20938e18b3430706e42934955a39aa42ec367a
szintia <kasziba.cintia@gmail.com>
2015-11-10 20:23:23 +0100

    add description of each member as final variable

--- project/kameleon/src/enumeration/cultureclub/OldBand.java
+++ project/kameleon/src/enumeration/cultureclub/OldBand.java
@@ -3,6 +3,12 @@ package enumeration.cultureclub;
 public class OldBand {
 	
 	CultureClub member;
+	public static final String SINGER = "singer";
+	public static final String PERCUSSION_INSTRUMENTS = "Percussion instruments";
+	public static final String GUITAR = "guitar";
+	public static final String BASS_GUITAR = "bass guitar";
+	public static final String LEAD_SINGER = "Lead singer";
+	
 	
 	public OldBand(CultureClub member) {
 		this.member = member;
@@ -10,17 +16,17 @@ public class OldBand {
 	
 	public void describeBand() {
 		switch(member) {
-			case BOY_GEORGE: System.out.println("Lead singer");
+			case BOY_GEORGE: System.out.println(SINGER);
 				break;
-			case JON_MOSS: System.out.println("Percussion instruments");
+			case JON_MOSS: System.out.println(PERCUSSION_INSTRUMENTS);
 				break;
-			case JOHN_SUEDE: System.out.println("guitar");
+			case JOHN_SUEDE: System.out.println(GUITAR);
 				break;
-			case MIKEY_CRAIG: System.out.println("bass guitar");
+			case MIKEY_CRAIG: System.out.println(BASS_GUITAR);
 				break;
-			case ROY_HAY: System.out.println("guitar");
+			case ROY_HAY: System.out.println(GUITAR);
 				break;
-			case SAM_BUTCHER: System.out.println("singer");
+			case SAM_BUTCHER: System.out.println(SINGER);
 				break;
 			default: System.out.println("There is not such member");
 		}
